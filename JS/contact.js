4338d23cae5a5c932b2f42db5fbc010b3139a295
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-04-27 14:25:41 +0300

    Add location funcion

--- JS/contact.js
+++ JS/contact.js
@@ -1,47 +1,38 @@
-function GEEKFORGEEKS() {
-  var name = document.forms["RegForm"]["Name"];
-  var email = document.forms["RegForm"]["EMail"];
-  var phone = document.forms["RegForm"]["Telephone"];
-  var what = document.forms["RegForm"]["Subject"];
-  var password = document.forms["RegForm"]["Password"];
-  var address = document.forms["RegForm"]["Address"];
+function towToTheRescue() {
+  var name = document.forms["contact"]["Name"];
+  var email = document.forms["contact"]["EMail"];
+  var phone = document.forms["contact"]["Telephone"];
+  var what = document.forms["contact"]["Subject"];
+  var address = document.forms["contact"]["Location"];
 
   if (name.value == "") {
-      window.alert("Please enter your name.");
-      name.focus();
-      return false;
+    alert("Please enter your name.");
+    name.focus();
+    return false;
   }
 
   if (address.value == "") {
-      window.alert("Please enter your address.");
-      address.focus();
-      return false;
+    alert("Please enter your address.");
+    address.focus();
+    return false;
   }
 
   if (email.value == "") {
-      window.alert(
-        "Please enter a valid e-mail address.");
-      email.focus();
-      return false;
+    alert("Please enter a valid e-mail address.");
+    email.focus();
+    return false;
   }
 
   if (phone.value == "") {
-      window.alert(
-        "Please enter your telephone number.");
-      phone.focus();
-      return false;
-  }
-
-  if (password.value == "") {
-      window.alert("Please enter your password");
-      password.focus();
-      return false;
+    alert("Please enter your telephone number.");
+    phone.focus();
+    return false;
   }
 
   if (what.selectedIndex < 1) {
-      alert("Please enter your course.");
-      what.focus();
-      return false;
+    alert("Please enter your county.");
+    what.focus();
+    return false;
   }
 
   return true;
