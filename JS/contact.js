1034b5e1a10aa038f424f95756a55daa5a665eb8
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-04-27 15:48:21 +0300

    Add alert message to contact

--- JS/contact.js
+++ JS/contact.js
@@ -1,39 +1,19 @@
-function towToTheRescue() {
-  var name = document.forms["contact"]["Name"];
-  var email = document.forms["contact"]["EMail"];
-  var phone = document.forms["contact"]["Telephone"];
-  var what = document.forms["contact"]["Subject"];
-  var address = document.forms["contact"]["Location"];
+$(document).ready(function(){
+  $("form#submit").submit(function(event){
+     event.preventDefault();
+    var name = $("input#name1").val();
+    var email = $("input#name2").val();
+    var phone = $("input#phone").val();
+    var location = $("input#location1").val();
+    var message = $("textarea#comment").val();
+    if ($("input#name1").val() && $("input#name2").val()&& $("input#phone").val()&& $("input#location1").val()){
+      alert (name + " " + "from" + " " + location + "" + ",Tow-To-The-Rescue has received your message. Our Technical team will reach out to you as soon as possible.");
+    }
+    else {
+      alert("Please enter a valid name, email and contact!");
+    }
 
-  if (name.value == "") {
-    alert("Please enter your name.");
-    name.focus();
-    return false;
-  }
+    
+  });
 
-  if (address.value == "") {
-    alert("Please enter your address.");
-    address.focus();
-    return false;
-  }
-
-  if (email.value == "") {
-    alert("Please enter a valid e-mail address.");
-    email.focus();
-    return false;
-  }
-
-  if (phone.value == "") {
-    alert("Please enter your telephone number.");
-    phone.focus();
-    return false;
-  }
-
-  if (what.selectedIndex < 1) {
-    alert("Please enter your county.");
-    what.focus();
-    return false;
-  }
-
-  return true;
-}
+});
