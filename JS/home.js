d33412460942666bf43971d9a773a7617760a66f
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-28 20:08:23 +0300

    created the hoover effect

---
+++ JS/home.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+  $(".over1").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text1").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text1").hide();
+  });
+  $(".over2").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text2").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text2").hide();
+  });
+  $(".over3").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text3").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text3").hide();
+  });
+  $(".over4").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text4").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text4").hide();
+  });
+  $(".over5").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text5").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text5").hide();
+  });
+  $(".over6").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text6").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text6").hide();
+  });
+  $(".over7").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text7").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text7").hide();
+  });
+  $(".over8").mouseover(function () {
+    $(this).css("opacity", "0.7");
+    $(".text8").show();
+  }).mouseout(function () {
+    $(this).css("opacity", "1");
+    $(".text8").hide();
+  });
+event.preventDefault();
+
+});
